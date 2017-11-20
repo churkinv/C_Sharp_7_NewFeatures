79926d5c3e7211e30ea1f1a5be7ce6096ab53389
churkinv <churkinv@gmail.com>
2017-11-20 18:01:28 +0200

    Some Language Enhancements

---
+++ C_Sharp_7_NewFeatureTests/LanguageEnhancementsTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C_Sharp_7_NewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_7_NewFeatures.Tests
+{
+    [TestClass()]
+    public class LanguageEnhancementsTests
+    {
+        LanguageEnhancements test = new LanguageEnhancements();
+
+        [TestMethod()]
+        public void GetBigNumberTest()
+        {
+
+            int actual = test.GetBigNumber(); // 1_234_567
+            int expected = 1234567;
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void SubstituteTest()
+        {
+            int [] numbers = { 2, 7, 1, 9, 12, 8, 15 };
+            ref int position = ref test.Substitute(12, numbers);
+
+            position = -12;
+
+            int actual = numbers[4]; // now it is 12
+            int expected = -12;
+
+            Assert.AreEqual(actual, expected);
+
+        }
+    }
+}
---
+++ C_Sharp_7_NewFeatures/LanguageEnhancements.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_7_NewFeatures
+{
+    public class LanguageEnhancements
+    {
+        public int GetBigNumber()
+        {
+            return 1_234_567; // and it will return 1234567
+        }
+
+        public ref int Substitute(int value, int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == value)
+                {
+                    return ref numbers[i];
+                }
+            }
+            throw new IndexOutOfRangeException("Not Found");
+        }     
+    }
+
+    public class Hero
+    {
+        public string Name { get; }
+
+        public Hero(string name) =>
+            Name = name ?? throw new ArgumentNullException();
+    }
+}
