749da9a1fa03c6e9f94fdd69bb039fdc3aa9eb9b
churkinv <churkinv@gmail.com>
2017-11-20 17:40:13 +0200

    Local Functions

---
+++ C_Sharp_7_NewFeatureTests/LocalFunctionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C_Sharp_7_NewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_7_NewFeatures.Tests
+{
+    [TestClass()]
+    public class LocalFunctionsTests
+    {
+        [TestMethod()]
+        public void FibonacciTest()
+        {
+            var test = new LocalFunctions();
+            int target = 6;
+
+            int actual = test.Fibonacci(target);
+            int expected = 13;
+            
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ C_Sharp_7_NewFeatures/LocalFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_7_NewFeatures
+{
+    public class LocalFunctions
+    {
+        public int Fibonacci(int x)
+        {
+            if (x < 0) throw new ArgumentException(
+                   "Must be at least 0", nameof(x));
+            return Fib(x).current;
+
+            (int current, int previous) Fib (int i)
+            {
+                if (i == 0) return (1, 0);
+                var (current, previous) = Fib(i - 1);
+                return (current + previous, current);
+            }
+        }
+    }
+}
